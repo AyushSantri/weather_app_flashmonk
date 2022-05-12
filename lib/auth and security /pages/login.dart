1c23a72e05bd1a4150384c4f292d4dcea5e38183
AyushSantri <ayushsantri222@gmail.com>
2022-05-12 17:21:56 +0530

    removed single child scroll view and added center widget in login body

--- lib/auth and security /pages/login.dart
+++ lib/auth and security /pages/login.dart
@@ -12,7 +12,7 @@ class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
-      body: SingleChildScrollView(child: LoginBody()),
+      body: LoginBody(),
     );
   }
 }
