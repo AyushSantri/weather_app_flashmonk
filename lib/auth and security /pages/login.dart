26bb2493e2a16a8801e4acb2bd577f2816d4d243
AyushSantri <ayushsantri222@gmail.com>
2022-05-12 17:17:55 +0530

    WRAPPED LOGIN body with single child scroll view

--- lib/auth and security /pages/login.dart
+++ lib/auth and security /pages/login.dart
@@ -11,10 +11,8 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return const SingleChildScrollView(
-      child: Scaffold(
-        body: LoginBody(),
-      ),
+    return const Scaffold(
+      body: SingleChildScrollView(child: LoginBody()),
     );
   }
 }
