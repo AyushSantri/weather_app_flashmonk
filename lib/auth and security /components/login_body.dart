6c0b73ca7a7f580f7d3fdcc8b96253fa36c040fe
AyushSantri <ayushsantri222@gmail.com>
2022-05-13 22:28:50 +0530

    added a image in login page

--- lib/auth and security /components/login_body.dart
+++ lib/auth and security /components/login_body.dart
@@ -19,68 +19,73 @@ class _LoginBodyState extends State<LoginBody> {
     var height = MediaQuery.of(context).size.height;
     var width = MediaQuery.of(context).size.width;
 
-    return Center(
-      child: Form(
-          key: _formKey,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.center,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(
-                width: width / 1.3,
-                child: TextFormField(
-                  keyboardType: TextInputType.emailAddress,
-                  key: const ValueKey('email'),
-                  style: const TextStyle(color: Colors.grey),
-                  decoration: InputDecoration(
-                    contentPadding: const EdgeInsets.symmetric(vertical: 17),
-                    hintText: "Email address",
-                    prefixIcon: const Icon(Icons.email),
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(25),
+    return SingleChildScrollView(
+      child: Center(
+        child: Form(
+            key: _formKey,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Container(
+                  margin: const EdgeInsets.only(top: 20, bottom: 30),
+                    child: Image.asset("assets/splashWeather.png")),
+                SizedBox(
+                  width: width / 1.3,
+                  child: TextFormField(
+                    keyboardType: TextInputType.emailAddress,
+                    key: const ValueKey('email'),
+                    style: const TextStyle(color: Colors.grey),
+                    decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.symmetric(vertical: 17),
+                      hintText: "Email address",
+                      prefixIcon: const Icon(Icons.email),
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25),
+                      ),
                     ),
+                    onSaved: (value) {
+                      _email = value.toString().trim();
+                    },
+                    validator: (value) {
+                      if (value?.isEmpty == true) {
+                        return "Please enter email";
+                      } else if (value?.contains('@') == false) {
+                        return "Enter a valid E-mail";
+                      }
+                      return null;
+                    },
                   ),
-                  onSaved: (value) {
-                    _email = value.toString().trim();
-                  },
-                  validator: (value) {
-                    if (value?.isEmpty == true) {
-                      return "Please enter email";
-                    } else if (value?.contains('@') == false) {
-                      return "Enter a valid E-mail";
-                    }
-                    return null;
-                  },
                 ),
-              ),
-              SizedBox(
-                width: width / 1.3,
-                child: TextFormField(
-                  keyboardType: TextInputType.visiblePassword,
-                  key: const ValueKey('password'),
-                  obscureText: true,
-                  style: const TextStyle(color: Colors.grey),
-                  decoration: InputDecoration(
-                    contentPadding: const EdgeInsets.symmetric(
-                        vertical: 17, horizontal: 20),
-                    hintText: "password",
-                    border: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(25),
+                SizedBox(
+                  width: width / 1.3,
+                  child: TextFormField(
+                    keyboardType: TextInputType.visiblePassword,
+                    key: const ValueKey('password'),
+                    obscureText: true,
+                    style: const TextStyle(color: Colors.grey),
+                    decoration: InputDecoration(
+                      contentPadding: const EdgeInsets.symmetric(
+                          vertical: 17, horizontal: 20),
+                      hintText: "password",
+                      border: OutlineInputBorder(
+                        borderRadius: BorderRadius.circular(25),
+                      ),
                     ),
+                    onSaved: (value) {
+                      _password = value!;
+                    },
+                    validator: (value) {
+                      if (value!.length < 6) {
+                        return "min length 6";
+                      }
+                      return null;
+                    },
                   ),
-                  onSaved: (value) {
-                    _password = value!;
-                  },
-                  validator: (value) {
-                    if (value!.length < 6) {
-                      return "min length 6";
-                    }
-                    return null;
-                  },
                 ),
-              ),
-            ],
-          )),
+              ],
+            )),
+      ),
     );
   }
 }
