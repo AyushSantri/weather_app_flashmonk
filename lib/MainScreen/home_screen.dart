de94b8fec0849e07fac8cb65dbe2ddd8352fea78
AyushSantri <ayushsantri222@gmail.com>
2022-05-14 04:18:18 +0530

    added a column in stack and added text in it

--- lib/MainScreen/home_screen.dart
+++ lib/MainScreen/home_screen.dart
@@ -8,6 +8,7 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
+  int temperature = 0;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -17,7 +18,16 @@ class _HomeScreenState extends State<HomeScreen> {
             height: MediaQuery.of(context).size.height,
             width: MediaQuery.of(context).size.width,
             child: Image.asset("assets/clear.png", fit: BoxFit.fill),
-          )
+          ),
+          Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Text("$temperature C", style: const TextStyle(color: Colors.white, fontSize: 60),)
+              ],
+            ),
+          ),
         ],
       ),
     );
