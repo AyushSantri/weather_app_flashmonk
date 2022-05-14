5777c7ca4d09c1e0935bdae84d1b23544d5d635e
AyushSantri <ayushsantri222@gmail.com>
2022-05-14 13:46:38 +0530

    added const in login.dart

--- lib/auth and security /pages/login.dart
+++ lib/auth and security /pages/login.dart
@@ -11,7 +11,7 @@ class Login extends StatefulWidget {
 class _LoginState extends State<Login> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
+    return const Scaffold(
       body: LoginBody(),
     );
   }
